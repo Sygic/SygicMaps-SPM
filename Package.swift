d8617f4aa41239c7e54351261fcaf8885bd0f4c1
SygicCoreTechUnitBot <SygicCoreTechUnitBot@users.noreply.github.com>
2026-08-19 12:09:59 +0200

    Publish version 28.4.21

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SygicMaps",
     platforms: [
-        .iOS(.v16)
+        .iOS(.v12)
     ],
     products: [
         .library(
@@ -15,7 +15,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/Sygic/SygicAuth-SPM", from: "3.0.0")
+        .package(url: "https://github.com/Sygic/SygicAuth-SPM", from: "2.0.0")
     ],
     targets: [
         .target(
@@ -29,8 +29,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "SygicMaps",
-            url: "https://public.repo.sygic.com/repository/maven-sygic-coretech-private/com/sygic/sdk/maps-ios/41.1.0-dev.505/maps-ios-41.1.0-dev.505.zip",
-            checksum: "a5c895879f7091c98f68a7b80546455b8dd91cf62a23e7b4d50447e1485ee699"
+            url: "https://public.repo.sygic.com/repository/maven-sygic-releases/com/sygic/sdk/maps-ios/28.4.21/maps-ios-28.4.21.zip",
+            checksum: "190f2d17bbbbb00f5da547e5cfda97d3f6c916fe02043576746161ee4dc24e7a"
         )
     ]
 )
